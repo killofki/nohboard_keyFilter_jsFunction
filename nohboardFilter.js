965b14ecfe5d620b263c0641410a130856a11c69
killofki <killofki@yahoo.co.kr>
2020-05-18 01:45:51 +0900

    closer

--- nohboardFilter.js
+++ nohboardFilter.js
@@ -6,7 +6,7 @@ function keyFilter( o, F ) {
 	let text = JSON .stringify(...[ o,, '\t' ]) 
 	
 	return textLiner( text ) 
-	} 
+	} // -- keyFilter() 
 
 function textLiner( t ) { 
 	// 200511 json sillofki style // killofk style 
@@ -14,20 +14,7 @@ function textLiner( t ) {
 	let prespace = pre .replace( /(\t+)((?!, ).*[\n\r]+)(?=\1, )/g, '$1  $2' ) 
 	let closer = prespace .replace( /(?<=([\n\r]|^)(\t+)(\t(?!\t).*[\n\r]+))(\2(?!\t)\})/g, '$2\t}' ) 
 	return closer 
-	} 
-
-function jsonKillofkiStyle( obj ) { 
-	let { stringify } = JSON 
-	jsonKillofkiStyle = obj => { 
-		let tabs = stringify(...[ obj,, '\t' ]) 
-		let pre = tabs .replace( /,([\n\r]+)(\t+)/g, '$1$2, ' ) 
-		let prespace = pre .replace( /(\t+)((?!, ).*[\n\r]+)(?=\1, )/g, '$1  $2' ) 
-		let closer = prespace .replace( /(?<=([\n\r]|^)(\t+)(\t(?!\t).*[\n\r]+))(\2(?!\t)\})/g, '$2\t}' ) 
-		return closer 
-		} // -- lazy() 
-	return jsonKillofkiStyle( obj ) // lazy 
-	} // -- jsonKillofkiStyle() 
-
+	} // -- textLiner() 
 
 /* sample 
 
